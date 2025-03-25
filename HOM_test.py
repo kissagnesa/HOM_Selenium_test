d48916b9ad7d8e94b27577ba98afcc156662a2f4
kissagnesa <sindzse88@gmail.com>
2025-03-25 11:15:33 +0100

    Registration test written

--- HOM_test.py
+++ HOM_test.py
@@ -5,3 +5,16 @@ import time
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 driver.set_window_size(1920, 1080)
+
+driver.get("https://rejtelyekhaza.netlify.app/")
+driver.save_screenshot("HOM_test.png")
+time.sleep(5)
+part=driver.find_element(By.LINK_TEXT, "Regisztráció")
+if part.is_displayed():
+    print("Az oldal megjelenik")
+else:
+    print("Az oldal nem jelenik meg")
+part.click()
+driver.save_screenshot("Regisztracio.png")
+time.sleep(5)
+driver.quit
