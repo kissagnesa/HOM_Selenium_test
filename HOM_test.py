0a90869c72c2627be4a4b9215e3ed2658e161425
kissagnesa <sindzse88@gmail.com>
2025-03-25 10:18:38 +0100

    Python test file created

---
+++ HOM_test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1920, 1080)
